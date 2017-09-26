5298a82f44c20d2b83e3f06f0c7cfa76a9ddd02a
Luke Policinski <luke.policinski@egov.com>
2017-09-26 10:21:50 -0400

    publish works

--- publish.sh
+++ publish.sh
@@ -2,10 +2,9 @@
 
 if [ -z "$GH_TOKEN" ]; then
     echo "You must set the GH_TOKEN environment variable."
-    echo "See README.md for more details."
+    echo "export GH_TOKEN=`<token>`"
     exit 1
 fi
 
 # This will build, package and upload the app to GitHub.
-#node_modules/.bin/build --win --mac -p always
-node_modules/.bin/build --mac -p always
+node_modules/.bin/build --win --mac -p always
