a967aeec61951166a68440d5d77a99b72fe77584
Luke Policinski <Luke@LukePOLO.com>
2017-09-26 18:14:39 -0400

    everything seems to be working for auto updating, need to test it and will remove debugging after testing versioning

--- src/main/index.js
+++ src/main/index.js
@@ -27,9 +27,9 @@ function createWindow() {
     width: 1000
   });
 
-  if (environment === "development") {
+  // if (environment === "development") {
     mainWindow.webContents.openDevTools();
-  }
+  // }
 
   mainWindow.loadURL(winURL);
 
@@ -65,12 +65,42 @@ import { autoUpdater } from "electron-updater";
 autoUpdater.autoDownload = true;
 
 autoUpdater.setFeedURL({
+    provider : 'github',
     owner: 'CodePier',
     repo: 'Mail-Trapper'
 })
 
-autoUpdater.on("update-downloaded", () => {
-  autoUpdater.quitAndInstall();
+autoUpdater.on('checking-for-update', function () {
+    sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', function (info) {
+    sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', function (info) {
+    sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', function (err) {
+    sendStatusToWindow('Error in auto-updater.');
+});
+
+autoUpdater.on('download-progress', function (progressObj) {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+    sendStatusToWindow('Update downloaded; will install in 1 seconds');
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+    setTimeout(function () {
+        autoUpdater.quitAndInstall();
+    }, 1000);
 });
 
 app.on("ready", () => {
@@ -78,3 +108,7 @@ app.on("ready", () => {
     autoUpdater.checkForUpdates();
   }
 });
+
+function sendStatusToWindow(message) {
+    console.log(message);
+}
